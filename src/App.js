e4f7f56b3e91a9ba2b5f8a64523920f9eeec696a
narayanbavisetti <narayan3119@gmail.com>
2022-08-23 23:50:08 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1 style={{textAlign: "center"}}>Acche se kaam karna madarchod</h1>
+   
+      </header>
+    </div>
+  );
+}
+
+export default App;
